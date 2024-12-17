1390fc1e6eca888dc7d679b113ad96380d2e92fe
Nicklas Rohman <Nicklas.Rohman@nexergroup.com>
2024-12-17 16:29:25 +0100

    start with FaceApi component

---
+++ facial-recognition/src/components/FaceApi.vue
@@ -0,0 +1,10 @@
+<template></template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "FaceApi",
+  setup() {},
+});
+</script>
+<style lang="css"></style>
