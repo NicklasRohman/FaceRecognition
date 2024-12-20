134e4a375052598006bf12a351b30d912fe7f31b
Nicklas Rohman <Nicklas.Rohman@nexergroup.com>
2024-12-20 09:03:49 +0100

    Add FaceApi

--- facial-recognition/src/App.vue
+++ facial-recognition/src/App.vue
@@ -1,25 +1,13 @@
 <script setup lang="ts">
-import Tensorflow from "./components/Tensorflow.vue";
+import FaceApi from "./components/FaceApi.vue";
 </script>
 
 <template>
   <div>
     <h1>This is faceRecognition with tensorflow</h1>
   </div>
+  <!--
   <Tensorflow />
+  -->
+  <FaceApi />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- facial-recognition/src/components/FaceApi.vue
+++ facial-recognition/src/components/FaceApi.vue
@@ -1,10 +1,44 @@
-<template></template>
-<script lang="ts">
-import { defineComponent } from "vue";
+<template>
+  <div>
+    <video ref="video" width="720" height="560" autoplay></video>
+  </div>
+</template>
 
-export default defineComponent({
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import * as faceapi from "face-api.js";
+
+defineOptions({
   name: "FaceApi",
-  setup() {},
+});
+
+const video = ref<HTMLVideoElement | null>(null);
+const MODELS_URL = "/models";
+
+const startVideo = () => {
+  if (video.value != null) {
+    navigator.mediaDevices
+      .getUserMedia({ video: {} })
+      .then((stream) => {
+        (video.value as HTMLVideoElement).srcObject = stream;
+      })
+      .catch((err) => console.error(err));
+  }
+};
+
+const loadModels = async () => {
+  try {
+    await faceapi.nets.tinyFaceDetector.loadFromUri(MODELS_URL);
+    await faceapi.nets.faceLandmark68Net.loadFromUri(MODELS_URL);
+    await faceapi.nets.faceRecognitionNet.loadFromUri(MODELS_URL);
+    await faceapi.nets.faceExpressionNet.loadFromUri(MODELS_URL);
+  } catch (error) {
+    console.error("Error loading models: ", error);
+  }
+};
+
+onMounted(() => {
+  startVideo();
+  loadModels();
 });
 </script>
-<style lang="css"></style>
